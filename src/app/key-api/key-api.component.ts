d9663c21de0babe84d196c02340727906eabbba4
Pavan Kumar Jorrigala <pavan.jorrigala@gmail.com>
2018-09-03 22:27:10 -0500

    US1000 : Added validation api key

--- src/app/key-api/key-api.component.ts
+++ src/app/key-api/key-api.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { KeyApiService } from '../key-api.service';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-key-api',
@@ -10,15 +11,27 @@ import { KeyApiService } from '../key-api.service';
 export class KeyApiComponent implements OnInit {
 
   OMDb_Key_Api = '';
+  isValidKey: boolean;
+  errorMessage: string;
 
-  constructor(private keyApiService: KeyApiService) { }
+  constructor(private keyApiService: KeyApiService,
+              private movieService: MovieService) { }
 
   ngOnInit() {
       this.OMDb_Key_Api = this.keyApiService.getOMDbKeyApi();
+      this.setOMDbKeyApi();
   }
 
   setOMDbKeyApi(): void {
-      this.keyApiService.setOMDbKeyApi(this.OMDb_Key_Api);
+      this.keyApiService
+      .setOMDbKeyApi(this.OMDb_Key_Api);
+
+      // validation the key
+      this.isValidKey = false;
+      this.movieService
+      .getMovie('alpha')
+      .subscribe(movie => {this.isValidKey = movie.imdbID.length > 0; },
+      error => {this.errorMessage = error.error.Error; console.log(error);});
   }
 
 }

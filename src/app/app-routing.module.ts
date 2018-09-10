8d1aa7d9081a3c5b534cc499bce6547e9886dc46
Pavan Kumar Jorrigala <pavan.jorrigala@gmail.com>
2018-09-09 21:43:47 -0500

    US1000 : Introduced HashLocationStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
     RouterModule
   ],
   imports: [
-      RouterModule.forRoot(routes)
+      RouterModule.forRoot(routes, {useHash: true})
   ]
 })
 export class AppRoutingModule { }
